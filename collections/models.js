18e78254e9c2ad27d4f4e5f5760ced1434e3ff8f
cinjon <cinjon.resnick@gmail.com>
2014-02-20 21:53:14 -0800

    models: small update to the specifications

--- collections/models.js
+++ collections/models.js
@@ -12,7 +12,7 @@ Services = new Meteor.Collection('services');
 
 // Inputs are sets of field inputs
 // field_name examples: list, e.g "Legal Status":["dropdown", ["Citizen", "Permanent Resident", ...]], "Zipcode":["number", "94131"]
-// {type:String (e.g. "patient"), dom:String (e.g. "dropdown"), name:String (e.g. "Disability"), placeholder=String (e.g. "25" for Age), list=[String] (e.g. ["Citizen", "Permanent Resident", "Temporary Visa", "Undocumented"] for Citizenship
+// {type:[String] (e.g. ["patient"]), dom:String (e.g. "dropdown"), name:String (e.g. "Disability"), placeholder=String (e.g. "25" for Age), list=[String] (e.g. ["Citizen", "Permanent Resident", "Temporary Visa", "Undocumented"] for Citizenship
 Inputs = new Meteor.Collection('inputs');
 
 // Flags are alerts that something has been flagged for changing
