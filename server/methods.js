ec25efa136a13b9eb61e071fcccd4241bebf2182
cinjon <cinjon.resnick@gmail.com>
2014-03-07 14:26:26 -0800

    methods: new stuff on the server for adding/removing services

--- server/methods.js
+++ server/methods.js
@@ -3,10 +3,26 @@ Meteor.methods({
     Resources.update({_id:resource_id}, {$set:{'locations.address.$i.spatial_location':{'lat':lat, 'lng':lng}}});
   },
 
+  add_service_to_resource: function(resource_id, service_id) {
+    //assuming service_id is a sub for now...
+    Resources.update({_id:resource_id}, {$addToSet:{sub_service_ids:service_id}});
+  },
+
   flag_resource: function(resource_id, user_id) {
     var timestamp = (new Date()).getTime();
     var counties = Resources.findOne({_id:resource_id}).locations.service_areas
     flag_id = Flags.insert({created_time:timestamp, resource_id:resource_id, user_id:user_id, open:true, closed_time:null, counties:service_areas});
+  },
+
+  mark_complete: function(resource_id, user_id) {
+    var timestamp = (new Date()).getTime();
+    Resources.update({_id:resource_id}, {$set:{needs_edit:false}})
+    Flags.update({resource_id:resource_id},
+                 {$set:
+                  {closed_time:timestamp, open:false,
+                   resource_id:resource_id, user_id:user_id}
+                 }
+                );
   },
 
   remove_all_services: function() {
@@ -16,4 +32,8 @@ Meteor.methods({
   remove_all_resources: function() {
     return Resources.remove({});
   },
+
+  remove_service_from_resource: function(resource_id, service_id) {
+    Resources.update({_id:resource_id}, {$pull:{sub_service_ids:service_id}})
+  }
 });
