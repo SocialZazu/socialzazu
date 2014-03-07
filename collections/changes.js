e800d029086bd2d49325c715564f0e49d3c085a8
cinjon <cinjon.resnick@gmail.com>
2014-03-07 14:24:58 -0800

    style + changes

--- collections/changes.js
+++ collections/changes.js
@@ -1,3 +1,3 @@
-// Changes are explicit changes made by someone with the permission level to do so
-// {created_time:Int, resource_id:String, field:String, service_id:String, old:String, new:String}
+// Changes are explicit changes to a resource or service made by someone with the permission level to do so
+// {created_time:Int, target_resource_id:String, field:String, target_service_id:String, old:String, new:String, service_adj:{removed:Boolean, _id:String}}
 Changes = new Meteor.Collection('changes');
