616b794a256002c1623a47fd80ae20a9f7c43f3f
cinjon <cinjon.resnick@gmail.com>
2014-03-21 12:11:34 -0700

    handlebars: is_required_field

--- client/lib/handlebars.js
+++ client/lib/handlebars.js
@@ -5,3 +5,10 @@ Handlebars.registerHelper("capitalize", function(str) {
 Handlebars.registerHelper("is_editing", function() {
   return is_editing_plus();
 });
+
+Handlebars.registerHelper("is_required_field", function(field) {
+  if (required_fields.indexOf(field.toLowerCase().trim().split(' ').join('_')) > -1) {
+    return '*';
+  }
+  return '';
+});
