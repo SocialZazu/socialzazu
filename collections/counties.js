a20119000693d28c4a512791e58e89fe715e6436
cinjon <cinjon.resnick@gmail.com>
2014-03-27 10:24:41 -0500

    collections: minor update to the description

--- collections/counties.js
+++ collections/counties.js
@@ -1,3 +1,3 @@
 // Counties are a model for a county. Comes with name and geo center.
-// {name:String, coordinates:{lat:Number, lng:Number}}
+// {name:String, coordinates:{lat:Number, lng:Number}, location_count:Int}
 Counties = new Meteor.Collection('counties');
